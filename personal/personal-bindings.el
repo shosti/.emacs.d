1f77091c225e0acb4be80369719ef67a6edd061a
Emanuel Evans <emanuel.evans@gmail.com>
2013-03-16 10:30:24 -0700

    Set up ctl-z keybindings

--- personal/personal-bindings.el
+++ personal/personal-bindings.el
@@ -1,32 +1,39 @@
-;;window management
+;; window management
 (global-set-key (kbd "M-3") 'split-window-horizontally)
 (global-set-key (kbd "M-2") 'split-window-vertically)
 (global-set-key (kbd "M-1") 'delete-other-windows)
 (global-set-key (kbd "M-0") 'delete-window)
 (global-set-key (kbd "M-o") 'other-window)
 
-;;misc
+;; misc
 (global-set-key (kbd "C-w") 'personal-backward-kill-word)
 (global-set-key "\C-x\C-z" 'ns-do-hide-emacs)
 (global-set-key "\C-x\C-m" 'smex)
 (global-set-key "\C-xg" 'magit-status)
-(global-set-key "\C-z" 'eshell)
 
-;;packages
+;; packages
 (global-set-key (kbd "C-<f6>") 'package-list-packages)
 
-;;paredit
+;; paredit
 (require 'paredit)
 (define-key paredit-mode-map "\C-w" 'personal-paredit-backward-kill-word)
 
-;;anything
+;; anything
 (global-set-key "\C-x\C-b" 'helm-for-files)
 (global-set-key "\M-y" 'helm-show-kill-ring)
 
-;;compile
+;; compile
 (global-set-key "\C-ck" 'compile)
 
 ;; miscellaneous
 (global-set-key (kbd "C-M-h") 'mark-defun) ; undo starter-kit binding
+
+;; ctl-z binding map
+(define-prefix-command 'ctl-z-map)
+(global-set-key "\C-z" 'ctl-z-map)
+(define-key ctl-z-map (kbd "C-1") 'eshell)
+(define-key ctl-z-map (kbd "C-1") 'display-fill-screen)
+(define-key ctl-z-map (kbd "C-2") 'display-left-half)
+(define-key ctl-z-map (kbd "C-3") 'display-right-half)
 
 (provide 'personal-bindings)
