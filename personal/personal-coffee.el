8b9b57ebe75d6c4c0f0c67d0553a6648e1b6a3a4
Emanuel Evans <emanuel.evans@gmail.com>
2013-03-15 18:36:09 -0700

    Configure coffee-mode

---
+++ personal/personal-coffee.el
@@ -0,0 +1,13 @@
+(defun set-up-coffee-mode ()
+  (run-hooks 'prog-mode-hook)
+  (setq coffee-command "coffee"))
+
+(add-hook 'coffee-mode-hook
+          'set-up-coffee-mode)
+
+(add-hook 'comint-mode-hook
+          '(lambda ()
+             (if (equal (buffer-name) "*CoffeeREPL*")
+                 (setq comint-process-echoes t))))
+
+(provide 'personal-coffee)
