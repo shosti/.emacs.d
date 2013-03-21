20ed0aca21fcb4d4c1fb773d03a4fec9f0617538
Emanuel Evans <emanuel.evans@gmail.com>
2013-03-21 08:51:28 -0700

    Work around bug in esk-ruby

--- personal/personal-ruby.el
+++ personal/personal-ruby.el
@@ -1,6 +1,9 @@
 (defun set-up-inf-ruby-mode ()
   (setq comint-process-echoes t))
 
+;; workaround bug in esk-ruby
+(defalias 'inf-ruby-keys 'inf-ruby-setup-keybindings)
+
 (add-hook 'inf-ruby-mode-hook
           'set-up-inf-ruby-mode)
 
