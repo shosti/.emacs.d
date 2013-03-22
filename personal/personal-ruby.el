a45663a924b62dda73011181f64f4e1045629b87
Emanuel Evans <emanuel.evans@gmail.com>
2013-03-22 00:50:11 -0700

    Add ruby-end

--- personal/personal-ruby.el
+++ personal/personal-ruby.el
@@ -1,16 +1,22 @@
 ;;; -*- lexical-binding: t -*-
 
 (personal-require-package 'starter-kit-ruby)
+(personal-require-package 'ruby-end)
 (personal-require-package 'rinari)
 (personal-require-package 'rvm)
 (personal-require-package 'mmm-mode 'melpa)
 
-(add-hook 'inf-ruby-mode-hook
-          'set-up-inf-ruby-mode)
-
 (eval-after-load 'ruby-mode
   '(progn
      (define-key ruby-mode-map (kbd "C-c v") 'ruby-load-file)))
+
+;; workaround for bug in starter-kit-ruby
+(defalias 'inf-ruby-keys 'inf-ruby-setup-keybindings)
+
+(defun set-up-ruby-mode ()
+  (ruby-end-mode 1))
+
+(add-hook 'ruby-mode-hook 'set-up-ruby-mode)
 
 ;; rvm
 (rvm-use-default)
