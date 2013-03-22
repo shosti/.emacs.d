ac3d9285e29f78e013c0b7fa782a556972da9a78
Emanuel Evans <emanuel.evans@gmail.com>
2013-03-22 00:41:07 -0700

    Add aliases for ack

--- personal/personal-ack.el
+++ personal/personal-ack.el
@@ -2,4 +2,9 @@
 
 (personal-require-package 'ack-and-a-half)
 
+(defalias 'ack 'ack-and-a-half)
+(defalias 'ack-same 'ack-and-a-half-same)
+(defalias 'ack-find-file 'ack-and-a-half-find-file)
+(defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
+
 (provide 'personal-ack)
