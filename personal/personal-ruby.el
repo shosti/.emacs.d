2c6b37e445917652905cbbf29f5b0df3eefac112
Emanuel Evans <emanuel.evans@gmail.com>
2013-03-22 17:04:32 -0700

    Update ruby configuration

--- personal/personal-ruby.el
+++ personal/personal-ruby.el
@@ -3,7 +3,8 @@
 (personal-require-package 'starter-kit-ruby)
 (personal-require-package 'ruby-end 'melpa)
 (personal-require-package 'ruby-tools 'melpa)
-(personal-require-package 'rinari)
+(personal-require-package 'ruby-compilation 'melpa)
+(personal-require-package 'rinari 'melpa)
 (personal-require-package 'rvm)
 (personal-require-package 'mmm-mode 'melpa)
 
@@ -19,16 +20,20 @@
 ;; hooks
 (defun set-up-ruby-mode ()
   (ruby-end-mode 1)
-  (ruby-tools-mode 1))
+  (ruby-tools-mode 1)
+  (electric-pair-mode 1))
 
 (add-hook 'ruby-mode-hook 'set-up-ruby-mode)
 
 (defun set-up-inf-ruby-mode ()
+  (ruby-tools-mode 1)
   (setq comint-process-echoes t))
 
 (add-hook 'inf-ruby-mode-hook 'set-up-inf-ruby-mode)
 
 ;; rvm
+(setq ruby-compilation-executable (expand-file-name "~/.rvm/bin/ruby"))
+(setq ruby-compilation-executable-rake (expand-file-name "~/.rvm/bin/rake"))
 (rvm-use-default)
 
 ;; erb
