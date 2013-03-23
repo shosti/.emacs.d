e79778c6accbad5f62a088e240d068ef79f65416
Emanuel Evans <emanuel.evans@gmail.com>
2013-03-22 17:04:18 -0700

    Keybindings for shell and term

--- personal/personal-shell.el
+++ personal/personal-shell.el
@@ -1,5 +1,10 @@
 ;;; -*- lexical-binding: t -*-
 
+(require 'personal-bindings)
+
 (add-to-list 'explicit-bash-args "--login")
+
+(global-set-key (kbd "C-z C-s") 'shell)
+(global-set-key (kbd "C-z C-t") 'ansi-term)
 
 (provide 'personal-shell)
