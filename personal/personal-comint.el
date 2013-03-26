05043acb6502bc1608630b1c9091ab67e4cad551
Emanuel Evans <emanuel.evans@gmail.com>
2013-03-26 12:28:25 -0700

    Set up yasnippet for use in comint-mode

--- personal/personal-comint.el
+++ personal/personal-comint.el
@@ -1,8 +1,12 @@
 ;;; -*- lexical-binding: t -*-
 
+(require 'comint)
+
 (setq comint-scroll-to-bottom-on-input t)
 (setq comint-scroll-show-maximum-output nil)
 
 (define-key comint-mode-map (kbd "C-a") 'comint-bol)
+(define-key comint-mode-map (kbd "<M-return>") nil)
+(define-key comint-mode-map (kbd "<S-return>") 'comint-accumulate)
 
 (provide 'personal-comint)
--- personal/personal-yasnippet.el
+++ personal/personal-yasnippet.el
@@ -9,6 +9,7 @@
 (yas-global-mode 1)
 
 (define-key yas-minor-mode-map (kbd "TAB") nil)
+(define-key yas-minor-mode-map (kbd "<tab>") nil)
 (define-key yas-minor-mode-map (kbd "M-RET") 'yas-expand)
 
 (provide 'personal-yasnippet)
