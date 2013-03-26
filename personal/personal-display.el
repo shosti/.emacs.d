86fa0602b7b859fb810284cbbcd13cc350ba1aae
Emanuel Evans <emanuel.evans@gmail.com>
2013-03-26 13:50:42 -0700

    Add rotate-windows function and keybinding

--- personal/personal-display.el
+++ personal/personal-display.el
@@ -13,7 +13,6 @@
 (blink-cursor-mode 1)
 (scroll-bar-mode 1)
 (menu-bar-mode 1)
-(setq)
 
 (when (boundp 'mouse-wheel-scroll-amount)
   (setq mouse-wheel-scroll-amount '(0.01)))
@@ -42,4 +41,31 @@
                              (height . 53)
                              (width . 82))))
 
+(defun personal-rotate-windows ()
+  "Rotate your windows"
+  (interactive)
+  (cond ((not (> (count-windows)1))
+         (message "You can't rotate a single window!"))
+        (t
+         (setq i 1)
+         (setq numWindows (count-windows))
+         (while  (< i numWindows)
+           (let* (
+                  (w1 (elt (window-list) i))
+                  (w2 (elt (window-list) (+ (% i numWindows) 1)))
+
+                  (b1 (window-buffer w1))
+                  (b2 (window-buffer w2))
+
+                  (s1 (window-start w1))
+                  (s2 (window-start w2))
+                  )
+             (set-window-buffer w1  b2)
+             (set-window-buffer w2 b1)
+             (set-window-start w1 s2)
+             (set-window-start w2 s1)
+             (setq i (1+ i)))))))
+
+;; Keybindings
+(global-set-key (kbd "C-c s") 'personal-rotate-windows)
 (provide 'personal-display)
