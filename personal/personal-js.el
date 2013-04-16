1fb844fe60c0cfc11387eef841d95cf127551f84
Emanuel Evans <emanuel.evans@gmail.com>
2013-04-15 19:53:35 -0700

    Clean up js comint filtering

--- personal/personal-js.el
+++ personal/personal-js.el
@@ -5,12 +5,16 @@
 
 (setq inferior-js-program-command "/usr/local/bin/node")
 
+(defun personal-js-comint-filter (output)
+  (replace-regexp-in-string ".*1G\.\.\..*5G" "..."
+                            (replace-regexp-in-string ".*1G.*3G" "> "
+                                                      output)))
+
 (defun set-up-inferior-js-mode ()
   (setq comint-process-echoes t)
   (add-to-list 'comint-preoutput-filter-functions
-               (lambda (output)
-                 (replace-regexp-in-string ".*1G\.\.\..*5G" "..."
-                                           (replace-regexp-in-string ".*1G.*3G" "> " output))))  )
+               'personal-js-comint-filter))
+
 (add-hook 'inferior-js-mode-hook 'set-up-inferior-js-mode t)
 
 (provide 'personal-js)
