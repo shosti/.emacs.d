d10df4768a592e0b74201962be9751400f9c3ab4
Emanuel Evans <emanuel.evans@gmail.com>
2013-04-16 08:32:37 -0700

    New wacspace for ruby/rinari

--- personal/personal-wacspace.el
+++ personal/personal-wacspace.el
@@ -1,29 +1,55 @@
-(personal-require-package 'wacspace 'melpa)
+(add-to-list 'load-path (expand-file-name "~/src/wacspace"))
 
 (require 'wacspace)
 (require 'personal-bindings)
 (require 'personal-display)
 (require 'scala-mode2)
 (require 'ensime)
+(require 'findr)
 
 (wacs-set-frame-fn full display-fill-screen)
 (wacs-set-frame-fn right display-right-half)
 (wacs-set-frame-fn left display-left-half)
 
+(defun personal-scala-test-file ()
+  (find-file (car (findr "Suite.scala$"
+                         (concat (wacs-project-dir)
+                                 "src/test/scala")))))
+
 (defwacspace (scala-mode)
   (:before (lambda ()
              (ensime-sbt-switch)
              (ensime-inf-switch)))
+  (:base-file ".ensime")
   (:default
    (:winconf 3winv)
    (:frame full)
-   (:aux1 (:buffer "*ensime-inferior-scala*"))
-   (:aux2 (:cmd (lambda ()
-                  (switch-to-buffer
-                   (--first
-                    (string-match-p "*ensime-sbt*"
-                                    (buffer-name it))
-                    (buffer-list)))))))
+   (:aux1 (:cmd personal-scala-test-file))
+   (:aux2 (:buffer "*ensime-sbt*")))
+  (:1
+   (:aux1 (:buffer "*ensime-inferior-scala*")))
+  (:2
+   (:winconf 2winh)
+   (:frame right)
+   (:aux2 (:buffer "*ensime-sbt*"))))
+
+(defwacspace (ruby-mode rinari-minor-mode)
+  (:before personal-rinari-guard)
+  (:default
+   (:winconf 3winv)
+   (:frame full)
+   (:aux1 (:cmd wacs-eshell))
+   (:aux2 (:buffer "*guard*")))
+  (:1
+   (:winconf 2winh)
+   (:frame right)))
+
+(defwacspace (emacs-lisp-mode)
+  (:default
+   (:winconf 3winv)
+   (:frame full)
+   (:aux1 (:cmd eshell))
+   (:aux2 (:buffer "*scratch*")))
   (:1
    (:winconf 2winh)
    (:frame right)))
