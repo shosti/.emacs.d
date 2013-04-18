48aa73e4580103f9cb0e42f7f5e05938da9d3c04
Emanuel Evans <emanuel.evans@gmail.com>
2013-04-17 22:14:34 -0700

    Enable ace-jump across windows

--- personal/p-ace-jump.el
+++ personal/p-ace-jump.el
@@ -5,4 +5,10 @@
 (global-set-key (kbd "C-c SPC") 'ace-jump-mode)
 (global-set-key (kbd "C-c C-SPC") 'ace-jump-mode)
 
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+
 (provide 'p-ace-jump)
+
+;;; p-ace-jump.el ends here
