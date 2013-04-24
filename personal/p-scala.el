7a070b50eccfc34689adf66ee4974607c9844851
Emanuel Evans <emanuel.evans@gmail.com>
2013-04-24 10:26:14 -0700

    Fixes to p-scala

--- personal/p-scala.el
+++ personal/p-scala.el
@@ -8,20 +8,19 @@
       (add-to-list 'load-path ensime-lib)
       (require 'ensime)
       (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-      (setq ensime-ac-override-settings nil)
-
-      (defun p-set-up-ensime ()
-        (add-to-list 'ac-sources 'ac-source-ensime-completions))
-
-      (add-hook 'scala-mode-hook 'p-set-up-ensime)
+      (setq ensime-ac-override-settings t)
 
       ;; Hack to fix ensime comint prompt
       (defadvice ensime-sbt (after set-process-echoes activate)
         (setq comint-process-echoes t)))))
 
+(defun p-set-up-scala ()
+  (electric-pair-mode))
+
 (eval-after-load 'scala-mode2
   '(progn
      (load-ensime)
+     (add-hook 'scala-mode-hook 'p-set-up-scala)
      (define-key scala-mode-map (kbd "RET") 'newline-and-indent)
      (define-key scala-mode-map (kbd "C-c C-c") 'ensime-inf-eval-buffer)))
 
