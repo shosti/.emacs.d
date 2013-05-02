5a48827f4f2c7e0201921d4e50a81f0cbb7bf052
Emanuel Evans <emanuel.evans@gmail.com>
2013-05-01 21:53:05 -0700

    New wacspaces

--- personal/p-wacspace.el
+++ personal/p-wacspace.el
@@ -1,17 +1,18 @@
-(add-to-list 'load-path (expand-file-name "~/src/wacspace"))
+(let ((wacsdir (expand-file-name "~/src/wacspace")))
+  (if (file-exists-p wacsdir)
+      (progn
+        (add-to-list 'load-path wacsdir)
+        (require 'wacspace))
+    (p-require-package 'wacspace 'melpa)))
 
-(require 'wacspace)
-(require 'p-bindings)
 (require 'p-display)
-(require 'scala-mode2)
-(require 'ensime)
 (require 'findr)
 
 ;;;;;;;;;;;;
 ;; Config ;;
 ;;;;;;;;;;;;
 
-
+(wacs-set-up-keys)
 (wacs-set-frame-fn full p-display-fill-screen)
 (wacs-set-frame-fn right p-display-right-half)
 (wacs-set-frame-fn left p-display-left-half)
@@ -21,10 +22,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;
 
 (defun p-find-first-matching-file (name dir)
-  (-> name
-    (findr dir)
-    (car)
-    (find-file)))
+  (--if-let (-> name
+              (findr dir)
+              (car))
+    (find-file it)
+    (wacs-eshell)))
 
 (defun p-scala-test-file ()
   (p-find-first-matching-file "Suite.scala$"
@@ -33,13 +35,90 @@
 
 (defun p-elisp-feature-file? ()
   (file-exists-p
-   (concat (p-trim-until-regexp default-directory
+   (concat (p-keep-until-regexp default-directory
                                 "features")
            "Carton")))
 
 ;;;;;;;;;;;;;;;
 ;; Wacspaces ;;
 ;;;;;;;;;;;;;;;
+
+;; My basic configuration scheme is as follows:
+;; Default: Full-screen overview (eshell, compilation, etc.)
+;; 1. Full-screen focus
+;; 2. Full-screen test
+;; 3. Full-screen repl
+;; 4. Right-screen focus
+;; 5. Right-screen repl
+;; 6. Left-screen focus
+;; 7. Left-screen repl
+
+(defwacspace (:default)
+  (:default
+   (:winconf 2winv)
+   (:frame full)
+   (:aux1 (:cmd wacs-eshell)))
+  (:1
+   (:winconf 1win))
+  (:2)
+  (:3)
+  (:4
+   (:frame right)
+   (:winconf 1win))
+  (:5
+   (:frame right)
+   (:winconf 2winh))
+  (:6
+   (:frame left)
+   (:winconf 1win))
+  (:7
+   (:frame left)
+   (:winconf 2winh)))
+
+(defwacspace (emacs-lisp-mode)
+  (:default
+   (:winconf 3winv)
+   (:aux2 (:buffer "*scratch*")))
+  (:2
+   (:winconf 2winv)
+   (:aux1 (:cmd
+           (lambda ()
+             (p-find-first-matching-file
+              "unit-tests.el"
+              (wacs-project-dir))))))
+  (:3
+   (:winconf 2winv)
+   (:aux1 (:cmd
+           (lambda ()
+             (p-find-first-matching-file
+              (concat (wacs-project-name)
+                      ".feature")
+              (wacs-project-dir)))))))
+
+(defwacspace (ruby-mode rinari-minor-mode)
+  (:before rinari-console)
+  (:after-switch rbenv-use-corresponding)
+  (:default
+   (:winconf 3winv)
+   (:aux2 (:cmd p-rinari-guard)))
+  (:2
+   (:winconf 2winv)
+   (:aux1 (:cmd rinari-find-rspec)))
+  (:3
+   (:winconf 2winv)
+   (:aux1 (:buffer "*rails console*"))) ;TODO: project-specific
+  (:5
+   (:aux1 (:buffer "*rails console*")))
+  (:7
+   (:aux1 (:buffer "*rails console*"))))
+
+(defwacsaliases ((html-erb-mode rinari-minor-mode)
+                 (haml-mode rinari-minor-mode)
+                 (js-mode rinari-minor-mode)
+                 (coffee-mode rinari-minor-mode)
+                 (scss-mode rinari-minor-mode)
+                 (css-mode rinari-minor-mode))
+  (ruby-mode rinari-minor-mode))
 
 (defwacspace (scala-mode)
   (:before (lambda ()
@@ -48,56 +127,18 @@
   (:base-file ".ensime")
   (:default
    (:winconf 3winv)
-   (:frame full)
    (:aux1 (:cmd p-scala-test-file))
    (:aux2 (:buffer "*ensime-sbt*")))
-  (:1
-   (:aux1 (:buffer "*ensime-inferior-scala*")))
   (:2
-   (:winconf 2winh)
-   (:frame right)
-   (:aux2 (:buffer "*ensime-sbt*"))))
-
-(defwacspace (ruby-mode rinari-minor-mode)
-  (:before (lambda ()
-             (p-rinari-guard)
-             (rinari-console)))
-  (:default
-   (:winconf 3winv)
-   (:frame full)
-   (:aux1 (:cmd wacs-eshell))
-   (:aux2 (:buffer "*guard*")))
-  (:1
-   (:aux1 (:buffer "*rails console*")))
-  (:2
-   (:winconf 2winh)
-   (:frame right)
-   (:aux1 (:cmd wacs-eshell)))
-  (:3
-   (:winconf 2winh)
-   (:frame right)
-   (:aux1 (:buffer "*guard*"))))
-
-(defwacspace (ruby-mode)
-  (:before (lambda ()
-             (run-ruby)))
-  (:default
    (:winconf 2winv)
-   (:frame full)
-   (:aux1 (:buffer "*ruby*")))
-  (:1
-   (:winconf 2winh)
-   (:frame right)))
-
-(defwacspace (emacs-lisp-mode)
-  (:default
-   (:winconf 3winv)
-   (:frame full)
-   (:aux1 (:cmd wacs-eshell))
-   (:aux2 (:buffer "*scratch*")))
-  (:1
-   (:winconf 2winh)
-   (:frame right)))
+   (:aux1 (:cmd p-scala-test-file)))
+  (:3
+   (:winconf 2winv)
+   (:aux1 (:buffer "*ensime-inferior-scala*")))
+  (:5
+   (:aux1 (:buffer "*ensime-inferior-scala*")))
+  (:7
+   (:aux1 (:buffer "*ensime-inferior-scala*"))))
 
 (defwacspace (feature-mode
               (lambda ()
@@ -117,15 +158,6 @@
   (:2
    (:winconf 2winh)
    (:frame right)))
-
-;;;;;;;;;;;;;;;;;
-;; Keybindings ;;
-;;;;;;;;;;;;;;;;;
-
-(define-key ctl-z-map (kbd "w") 'wacspace)
-(define-key ctl-z-map (kbd "C-w") 'wacspace)
-(define-key ctl-z-map (kbd "s") 'wacspace-save)
-(define-key ctl-z-map (kbd "C-s") 'wacspace-save)
 
 (provide 'p-wacspace)
 
