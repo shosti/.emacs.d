783582238b2277e8fa7be8a16e492a18732b747c
Emanuel Evans <emanuel.evans@gmail.com>
2013-07-11 17:03:50 -0700

    Smarter BOL from Emacs Redux

--- personal/p-editing.el
+++ personal/p-editing.el
@@ -87,6 +87,30 @@
         (goto-line (read-number "Goto line: ")))
     (linum-mode -1)))
 
+;; Thanks to Emacs Redux
+(defun p-smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
 ;;;;;;;;;;;;;;;;;
 ;; Keybindings ;;
 ;;;;;;;;;;;;;;;;;
@@ -104,6 +128,7 @@
 (global-set-key (kbd "C-S-b") 'p-fast-backward-char)
 (global-set-key (kbd "C-c d") 'p-duplicate-line)
 (global-set-key (kbd "C-x \\") 'align-regexp)
+(global-set-key (kbd "C-a") 'p-smarter-move-beginning-of-line)
 
 (provide 'p-editing)
 
