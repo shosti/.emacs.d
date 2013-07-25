65318c004e7f4b789e987a851f540114a80a9bec
Emanuel Evans <emanuel.evans@gmail.com>
2013-07-25 16:34:11 -0700

    Auto-indent in erb

--- personal/p-html.el
+++ personal/p-html.el
@@ -2,6 +2,10 @@
   '(progn
      (define-key html-mode-map (kbd "RET") 'newline-and-indent)))
 
+(eval-after-load 'mmm-erb
+  '(progn
+     (define-key html-erb-mode-map (kbd "RET") 'newline-and-indent)  ))
+
 (provide 'p-html)
 
 ;;; p-html.el ends here
