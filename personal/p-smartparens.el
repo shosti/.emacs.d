242e3e45f3f6f6e8e6d68d2123471a561fc60f73
Emanuel Evans <emanuel.evans@gmail.com>
2013-07-26 13:36:07 -0700

    Tweak smartparens config

--- personal/p-smartparens.el
+++ personal/p-smartparens.el
@@ -1,6 +1,12 @@
 (p-require-package 'smartparens)
 
 (require 'smartparens-config)
+
+(setq sp-highlight-wrap-overlay nil
+      sp-highlight-pair-overlay nil
+      sp-highlight-wrap-tag-overlay nil
+      sp-base-key-bindings 'paredit)
+
 (smartparens-global-mode 1)
 
 (provide 'p-smartparens)
