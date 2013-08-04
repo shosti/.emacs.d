e09afaf8d34ed9876eff7510c882d666d43c5699
Emanuel Evans <emanuel.evans@gmail.com>
2013-08-03 21:53:33 -0700

    Fix git-gutter config

--- personal/p-git-gutter.el
+++ personal/p-git-gutter.el
@@ -2,9 +2,17 @@
 
 (p-require-package 'git-gutter 'melpa)
 
-(require 'git-gutter)
-
 (global-git-gutter-mode 1)
-(global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
+
+(setq git-gutter:update-hooks '(after-save-hook after-revert-hook))
+(setq git-gutter:disabled-modes '(ediff-mode))
+
+(global-set-key (kbd "C-c g") 'git-gutter:toggle)
+(global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
+(global-set-key (kbd "C-x v p") 'git-gutter:previous-hunk)
+(global-set-key (kbd "C-x v n") 'git-gutter:next-hunk)
+(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
 
 (provide 'p-git-gutter)
+
+;;; p-git-gutter.el ends here
