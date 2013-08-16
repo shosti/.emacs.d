f14ccc72f3321657c66750fe1cfb8f188e6d5df3
Emanuel Evans <emanuel.evans@gmail.com>
2013-08-16 10:23:45 -0700

    Start using gnus for reading blogs

--- personal/p-gnus.el
+++ personal/p-gnus.el
@@ -2,12 +2,13 @@
 
 (require 'p-options)
 
-(defadvice gnus (before load-passwords activate)
-  (p-load-password-file))
-
 (setq user-mail-address "emanuel.evans@gmail.com")
 (setq user-full-name "Emanuel Evans")
 
 (setq gnus-select-method '(nntp "news.gmane.org"))
 
+(setq gnus-startup-file (concat user-emacs-directory ".newsrc"))
+
 (provide 'p-gnus)
+
+;;; p-gnus.el ends here
