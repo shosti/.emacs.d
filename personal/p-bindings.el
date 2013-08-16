54fad62d838689a858f2dd4ee94297f370795009
Emanuel Evans <emanuel.evans@gmail.com>
2013-08-16 10:25:31 -0700

    Helm binding that's nicer for split-keyboard

--- personal/p-bindings.el
+++ personal/p-bindings.el
@@ -35,6 +35,7 @@
 ;;;;;;;;;;;;;;
 
 (global-set-key (kbd "C-x C-b") 'helm-for-files)
+(global-set-key (kbd "C-x C-n") 'helm-for-files)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (eval-after-load 'helm
   '(define-key helm-map (kbd "C-w") nil))
