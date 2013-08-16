e152c1e7b7850aa04375413375533e309321ed8d
Emanuel Evans <emanuel.evans@gmail.com>
2013-08-16 10:26:36 -0700

    Twitter!

---
+++ personal/p-twitter.el
@@ -0,0 +1,9 @@
+(p-require-package 'twittering-mode 'melpa)
+
+(setq twittering-use-master-password t
+      twittering-icon-mode t
+      twittering-use-icon-storage t)
+
+(provide 'p-twitter)
+
+;;; p-twitter.el ends here
