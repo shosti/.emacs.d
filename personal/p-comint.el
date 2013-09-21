ba5cd71b48bb0c039bd9d6dfd58bc4dae32deb26
Emanuel Evans <emanuel.evans@gmail.com>
2013-09-21 13:09:22 -0700

    Minor cleanup of p-comint.el

--- personal/p-comint.el
+++ personal/p-comint.el
@@ -2,9 +2,9 @@
 
 (require 'comint)
 
-(setq comint-scroll-to-bottom-on-input t)
-(setq comint-scroll-to-bottom-on-output t)
-(setq comint-scroll-show-maximum-output nil)
+(setq comint-scroll-to-bottom-on-input t
+      comint-scroll-to-bottom-on-output t
+      comint-scroll-show-maximum-output nil)
 
 (define-key comint-mode-map (kbd "C-a") 'comint-bol)
 (define-key comint-mode-map (kbd "<M-return>") nil)
