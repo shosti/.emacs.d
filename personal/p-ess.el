d81716e48f8f8975f5b238ca9b5e63a2935c376e
Emanuel Evans <emanuel.evans@gmail.com>
2013-09-21 13:00:58 -0700

    Autoload ess

--- personal/p-ess.el
+++ personal/p-ess.el
@@ -1,10 +1,5 @@
-(defun p-load-ess ()
-  (interactive)
-  (let ((ess-lib (concat user-emacs-directory "opt/ess/lisp")))
-    (when (file-exists-p ess-lib)
-      (unless (-contains? load-path ess-lib)
-        (add-to-list 'load-path ess-lib)
-        (require 'ess-site)))))
+(add-to-list 'load-path (concat user-emacs-directory "opt/ess"))
+(load-library "ess-autoloads")
 
 (provide 'p-ess)
 
