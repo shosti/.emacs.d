a392f08805c25420f25bc6b84a223f508ec6b393
Emanuel Evans <emanuel.evans@gmail.com>
2013-09-29 21:50:31 -0700

    Fix ess autoscrolling
    I hate autoscrolling sooo much!!!!!

--- personal/p-ess.el
+++ personal/p-ess.el
@@ -1,6 +1,17 @@
 (add-to-list 'load-path (concat user-emacs-directory "opt/ess"))
 (load-library "ess-autoloads")
 
+;;;;;;;;;;;
+;; HACKS ;;
+;;;;;;;;;;;
+
+;; WHY DO SO MANY INFERIOR MODES INSIST ON SCROLLING TO THE
+;; BOTTOM?????
+(eval-after-load 'ess-mode
+  '(progn
+     (defadvice inferior-ess-input-sender (after recenter activate)
+       (recenter))))
+
 (provide 'p-ess)
 
 ;;; p-ess.el ends here
