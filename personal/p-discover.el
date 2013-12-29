8ddc33ae59733732d0fa409641cc98f3f599f33d
Emanuel Evans <emanuel.evans@gmail.com>
2013-12-29 13:04:57 -0800

    Start using discover.el

---
+++ personal/p-discover.el
@@ -0,0 +1,12 @@
+(require 'p-evil)
+
+(p-require-package 'makey 'melpa)
+(p-require-package 'discover 'melpa)
+
+(global-discover-mode 1)
+
+(add-to-list 'evil-emacs-state-modes 'makey-key-mode)
+
+(provide 'p-discover)
+
+;;; p-discover.el ends here
