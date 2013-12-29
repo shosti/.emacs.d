80a7322b3daaa2f19f56952a8de9a7eb3092b71e
Emanuel Evans <emanuel.evans@gmail.com>
2013-12-29 12:36:45 -0800

    Add stabby lambda to ruby pretty-mode

--- personal/p-pretty-mode.el
+++ personal/p-pretty-mode.el
@@ -4,6 +4,10 @@
 
 (require 'pretty-mode)
 
+(pretty-deactivate-groups '(:logic) '(ruby-mode))
+(pretty-activate-groups '(:greek))
+(pretty-add-keywords 'ruby-mode '(("->" . ?\u03BB)))
+
 (global-pretty-mode 1)
 
 (provide 'p-pretty-mode)
