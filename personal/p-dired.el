63d9807c30c80265089094f8b36ab3d565423831
Emanuel Evans <emanuel.evans@gmail.com>
2013-12-29 13:05:12 -0800

    Some fixes for dired

---
+++ personal/p-dired.el
@@ -0,0 +1,12 @@
+(require 'p-evil)
+(require 'p-discover)
+(require 'dired)
+
+(evil-define-key 'normal dired-mode-map (kbd "?")
+  'makey-key-mode-popup-dired)
+(define-key dired-mode-map (kbd "M-o") nil)
+(define-key dired-mode-map (kbd "C-x o") 'dired-omit-mode)
+
+(provide 'p-dired)
+
+;;; p-dired.el ends here
