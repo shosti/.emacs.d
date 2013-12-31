796ecbd1eb3263ccf5472f76cd955724dbc387b1
Emanuel Evans <emanuel.evans@gmail.com>
2013-12-31 14:41:49 -0800

    Turn on linum-mode by default

--- personal/p-programming.el
+++ personal/p-programming.el
@@ -8,4 +8,9 @@
 (defun set-up-auto-indent ()
   (local-set-key (kbd "RET") 'newline-and-indent))
 
+(defun p-set-up-prog-mode ()
+  (linum-mode 1))
+
+(add-hook 'prog-mode-hook 'p-set-up-prog-mode)
+
 (provide 'p-programming)
