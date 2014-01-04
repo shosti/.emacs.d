4485927746ce4c100e08ea6c6541300fb47985dc
Emanuel Evans <emanuel.evans@gmail.com>
2014-01-04 13:10:53 -0800

    Add evil-nerd-commenter

---
+++ personal/p-evil-nerd-commenter.el
@@ -0,0 +1,10 @@
+(p-require-package 'evil-nerd-commenter)
+
+(require 'evil-nerd-commenter)
+(require 'p-leader)
+
+(evilnc-default-hotkeys)
+
+(provide 'p-evil-nerd-commenter)
+
+;;; p-evil-nerd-commenter.el ends here
