b4e0332ec8df7ea421649ca7c5cbae01ad13de13
Emanuel Evans <emanuel.evans@gmail.com>
2014-01-05 23:17:33 -0800

    Speed up yasnippet a bit
    It was double loading

--- personal/p-yasnippet.el
+++ personal/p-yasnippet.el
@@ -5,7 +5,6 @@
 (require 'yasnippet)
 
 (setq yas-snippet-dirs (list (concat user-emacs-directory "snippets")))
-(yas-reload-all)
 (yas-global-mode 1)
 
 (define-key yas-minor-mode-map (kbd "TAB") nil)
@@ -13,3 +12,5 @@
 (define-key yas-minor-mode-map (kbd "M-RET") 'yas-expand)
 
 (provide 'p-yasnippet)
+
+;;; p-yasnippet.el ends here
