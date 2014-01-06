ae68c4cc9e3698075bd74ef00be5bed525c48e45
Emanuel Evans <emanuel.evans@gmail.com>
2014-01-05 17:53:26 -0800

    Remove smarter-bol function
    No longer needed/used thanks to Evil

--- personal/p-editing.el
+++ personal/p-editing.el
@@ -95,30 +95,6 @@
         (goto-line (read-number "Goto line: ")))
     (linum-mode -1)))
 
-;; Thanks to Emacs Redux
-(defun p-smarter-move-beginning-of-line (arg)
-  "Move point back to indentation of beginning of line.
-
-Move point to the first non-whitespace character on this line.
-If point is already there, move to the beginning of the line.
-Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-
-If ARG is not nil or 1, move forward ARG - 1 lines first.  If
-point reaches the beginning or end of the buffer, stop there."
-  (interactive "^p")
-  (setq arg (or arg 1))
-
-  ;; Move lines first
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
-
-  (let ((orig-point (point)))
-    (back-to-indentation)
-    (when (= orig-point (point))
-      (move-beginning-of-line 1))))
-
 ;;;;;;;;;;;;;;;;;
 ;; Keybindings ;;
 ;;;;;;;;;;;;;;;;;
@@ -136,7 +112,6 @@ point reaches the beginning or end of the buffer, stop there."
 (global-set-key (kbd "C-S-b") 'p-fast-backward-char)
 (global-set-key (kbd "C-c d") 'p-duplicate-line)
 (global-set-key (kbd "C-x \\") 'align-regexp)
-(global-set-key (kbd "C-a") 'p-smarter-move-beginning-of-line)
 (global-set-key (kbd "C-S-k") 'p-delete-current-line)
 (global-set-key " " 'just-one-space)
 
