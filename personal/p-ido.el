f63e298142547bbe6b2bb92417d09b427475676d
Emanuel Evans <emanuel.evans@gmail.com>
2014-01-06 20:13:03 -0800

    Delete starter-kit-misc

--- personal/p-ido.el
+++ personal/p-ido.el
@@ -3,7 +3,18 @@
 
 (require 'p-leader)
 
+(ido-mode t)
+(ido-ubiquitous t)
 (flx-ido-mode 1)
+
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-auto-merge-work-directories-length nil
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-use-virtual-buffers t
+      ido-handle-duplicate-virtual-buffers 2
+      ido-max-prospects 10)
 
 (defun p-ido-setup-keybindings ()
   (define-key ido-file-completion-map
