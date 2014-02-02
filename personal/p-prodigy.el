61b2241d9fda1a032d603f14102a797e41a726fe
Emanuel Evans <emanuel.evans@gmail.com>
2014-02-02 13:26:49 -0800

    Get prodigy working nicely

--- personal/p-prodigy.el
+++ personal/p-prodigy.el
@@ -1,10 +1,17 @@
 ;;; -*- lexical-binding: t -*-
 
-(p-require-package 'prodigy 'melpa)
+(p-require-package 'prodigy 'local)
 
 (require 'prodigy)
+(require 'p-leader)
+(require 'p-evil)
 
-(p-load-private "prodigy.el")
+(setq prodigy-view-truncate-by-default nil)
+(p-load-private "prodigy-settings.el")
+(add-to-list 'evil-emacs-state-modes 'prodigy-mode)
+(evil-add-hjkl-bindings prodigy-mode-map)
+
+(p-set-leader-key "y" 'prodigy)
 
 (provide 'p-prodigy)
 
