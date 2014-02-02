a40a6bd08788ab2ad8552d38b67acd7f97bf145d
Emanuel Evans <emanuel.evans@gmail.com>
2014-02-02 13:19:45 -0800

    Add a bunch of testing libraries

---
+++ personal/p-testing.el
@@ -0,0 +1,9 @@
+(p-require-package 'ecukes 'melpa)
+(p-require-package 'async 'melpa)
+(p-require-package 'el-mock 'melpa)
+(p-require-package 'ert-async 'melpa)
+(p-require-package 'commander 'melpa)
+
+(provide 'p-testing)
+
+;;; p-testing.el ends here
