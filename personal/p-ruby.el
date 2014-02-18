824c80f035baeeb6fd162d51770a874cf1646e28
Emanuel Evans <emanuel.evans@gmail.com>
2014-02-17 20:04:19 -0800

    Switch to web-mode for erb

--- personal/p-ruby.el
+++ personal/p-ruby.el
@@ -132,22 +132,6 @@ window."
 
 (global-rinari-mode 1)
 
-;;;;;;;;;
-;; ERB ;;
-;;;;;;;;;
-
-(require 'mmm-auto)
-
-(setq mmm-global-mode 'auto)
-
-(mmm-add-mode-ext-class 'html-erb-mode "\\.html\\.erb\\'" 'erb)
-(mmm-add-mode-ext-class 'html-erb-mode "\\.jst\\.ejs\\'" 'ejs)
-(mmm-add-mode-ext-class 'html-erb-mode nil 'html-js)
-(mmm-add-mode-ext-class 'html-erb-mode nil 'html-css)
-
-(add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . html-erb-mode))
-(add-to-list 'auto-mode-alist '("\\.jst\\.ejs\\'"  . html-erb-mode))
-
 ;;;;;;;;;;;;;;;;;
 ;; Keybindings ;;
 ;;;;;;;;;;;;;;;;;
---
+++ personal/p-web-mode.el
@@ -0,0 +1,7 @@
+(p-require-package 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+
+(provide 'p-web-mode)
+
+;;; p-web-mode.el ends here
