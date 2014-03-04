c2e540490b90d0e4881a64511284638465de92c1
Emanuel Evans <emanuel.evans@gmail.com>
2014-03-03 21:11:55 -0800

    Don't use smartparens with web-mode
    Was causing double-bracket inserts. Maybe eventually I'll get them to
    play nicely.

--- personal/p-web-mode.el
+++ personal/p-web-mode.el
@@ -2,6 +2,11 @@
 
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 
+(defun p-set-up-web-mode ()
+  (smartparens-mode 0))
+
+(add-hook 'web-mode-hook 'p-set-up-web-mode)
+
 (provide 'p-web-mode)
 
 ;;; p-web-mode.el ends here
