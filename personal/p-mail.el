91bde27b6a28933c3365d80eebd43ea447f7ce1f
Emanuel Evans <emanuel.evans@gmail.com>
2014-08-10 14:48:44 -0700

    Clean up/improve mail configuration

--- personal/p-mail.el
+++ personal/p-mail.el
@@ -5,21 +5,38 @@
 
 (defvar p-mu4e-account-alist)
 
-(p-load-private "mail-settings.el")
-
-(require 'smtpmail)
-
-(setq message-send-mail-function 'message-send-mail-with-sendmail
-      sendmail-program "/usr/local/bin/msmtp"
-      message-sendmail-f-is-evil t
-      message-sendmail-extra-arguments '("--read-envelope-from")
-      message-kill-buffer-on-exit t)
-
 (defun p-mail-set-up? ()
   (and (executable-find "mu")
      (executable-find "msmtp")
      (executable-find "offlineimap")
-     (ignore-errors (require 'mu4e-autoload))))
+     (file-exists-p (expand-file-name "~/Maildir"))
+     (--any? (s-contains? "mu4e" it) load-path)))
+
+(defun p-config-mu4e ()
+  (p-load-private "mail-settings.el")
+
+  (require 'smtpmail)
+
+  (setq message-send-mail-function 'message-send-mail-with-sendmail
+        sendmail-program "/usr/local/bin/msmtp"
+        message-sendmail-f-is-evil t
+        message-sendmail-extra-arguments '("--read-envelope-from")
+        message-kill-buffer-on-exit t
+        mu4e-sent-messages-behavior 'delete
+        mu4e-get-mail-command "offlineimap")
+
+  (add-hook 'mu4e-compose-pre-hook 'p-mu4e-set-account)
+
+  (--each (list mu4e-main-mode-map mu4e-headers-mode-map)
+    (evil-add-hjkl-bindings it 'emacs
+      "J" 'mu4e~headers-jump-to-maildir)))
+
+(if (p-mail-set-up?)
+    (progn (autoload 'mu4e "mu4e" "" t)
+           (eval-after-load 'mu4e '(p-config-mu4e)))
+  (defun mu4e ()
+    (interactive)
+    (error "mu4e not installed on this system")))
 
 ;; Stolen from the mu4e manual
 (defun p-mu4e-set-account ()
@@ -40,23 +57,7 @@
               account-vars)
       (error "No email account found"))))
 
-(if (p-mail-set-up?)
-    (eval-after-load 'mu4e
-      '(progn
-         (setq mu4e-sent-messages-behavior 'delete
-               mu4e-get-mail-command "offlineimap")
-         (add-hook 'mu4e-compose-pre-hook 'p-mu4e-set-account)
-
-         (--each (list mu4e-main-mode-map mu4e-headers-mode-map)
-           (evil-add-hjkl-bindings it 'emacs
-             "J" 'mu4e~headers-jump-to-maildir))))
-
-  (defun mu4e ()
-    (interactive)
-    (error "mu4e not installed on this system")))
-
 (p-set-leader-key "M" 'mu4e)
-
 
 (provide 'p-mail)
 
