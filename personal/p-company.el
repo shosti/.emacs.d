45f015206704c9b0922844183ecbc9293169826e
Emanuel Evans <emanuel.evans@gmail.com>
2014-08-15 22:15:48 -0700

    Vastly improve company-mode keybindings

--- personal/p-company.el
+++ personal/p-company.el
@@ -1,13 +1,29 @@
 (p-require-package 'company)
 
+;; No point autoloading since it's enabled globally
 (require 'company)
 
-(setq company-idle-delay 0)
+(setq company-idle-delay 0
+      company-require-match nil)
 
 (add-hook 'after-init-hook 'global-company-mode)
 
+;; Use control for all special keys, and make company non-intrusive
 (define-key company-active-map (kbd "C-n") 'company-select-next)
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "C-w") nil)
+(define-key company-active-map (kbd "C-d") 'company-show-location)
+(define-key company-active-map (kbd "C-RET") 'company-complete-selection)
+(define-key company-active-map [C-return] 'company-complete-selection)
+(define-key company-active-map (kbd "RET") nil)
+(define-key company-active-map [return] nil)
+(-dotimes 10
+  (lambda (i)
+    (define-key company-active-map (vector (+ (aref (kbd "M-0") 0) i)) nil)
+    (define-key company-active-map (vector (+ (aref (kbd "C-0") 0) i))
+      `(lambda ()
+         (interactive)
+         (company-complete-number ,(if (zerop i) 10 i))))))
 
 (setq company-global-modes
       '(not eshell-mode))
