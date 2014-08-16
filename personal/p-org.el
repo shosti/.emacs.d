220bda865a737c2e689a1eb5c1eace938114af96
Emanuel Evans <emanuel.evans@gmail.com>
2014-08-15 22:08:39 -0700

    Nifty little function for evaluating org lisp blocks

--- personal/p-org.el
+++ personal/p-org.el
@@ -2,6 +2,27 @@
 
 (setq org-startup-indented t)
 
+(defun p-eval-text-block ()
+  "Quick hack to evaluate and insert values of Elisp code blocks.
+
+This function assumes that for the code block under the block is
+split into two sections, first the definitions (whose values
+shouldn't be printed) and then the example values."
+  (interactive)
+  (save-excursion
+    (let ((start (region-beginning))
+          (end (region-end)))
+      (goto-char start)
+      (let ((definitions-end
+              (save-excursion (re-search-forward "^$"))))
+        (while (< (point) end)
+          (forward-sexp)
+          (let ((val (eval (preceding-sexp))))
+            (when (> (point) definitions-end)
+              (insert (format " ; => %S" val))))))
+      (goto-char start)
+      (align-regexp start end "\\(\\s-*\\); =>"))))
+
 (defun p-set-up-org-mode ()
   (auto-fill-mode 1)
   (flyspell-mode 1))
