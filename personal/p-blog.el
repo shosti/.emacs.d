cdf1f314489898d28352954106c74a3ff1708cbe
Emanuel Evans <emanuel.evans@gmail.com>
2014-08-23 12:18:02 -0700

    EXPERIMENTAL: Blogging with org

---
+++ personal/p-blog.el
@@ -0,0 +1,16 @@
+(p-require-package 'htmlize 'melpa)
+(p-require-package 'mustache 'melpa)
+(p-require-package 'org-page 'melpa)
+
+;; Let's hope this gets fixed upstream
+(autoload 'op/do-publication "org-page" "" t)
+
+(p-configure-feature org-page
+  (setq op/site-domain "https://shosti.github.io"
+        op/repository-directory (expand-file-name "~/blog")
+        op/personal-github-link "https://github.com/shosti"
+        op/personal-disqus-shortname "shosti"))
+
+(provide 'p-blog)
+
+;;; p-blog.el ends here
