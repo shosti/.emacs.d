d455934a35d0e2b9354c0b483436538694ab1804
Emanuel Evans <emanuel.evans@gmail.com>
2014-08-29 15:32:26 -0700

    Delete a bunch of obsolete clojure stuff
    Also some minor tweaks

--- personal/p-clojure.el
+++ personal/p-clojure.el
@@ -15,10 +15,7 @@
 ;; Config ;;
 ;;;;;;;;;;;;
 
-(add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
-
 (p-configure-feature cider
-  (setq cider-popup-stacktraces nil)
   (font-lock-add-keywords
    'nrepl-mode
    '(("(\\|)\\|\\[\\|\\]\\|{\\|}" . 'p-paren-face))))
@@ -36,60 +33,9 @@
 
 (setq evil-motion-state-modes
       (append '(cider-docview-mode
-                cider-popup-buffer-mode)
+                cider-popup-buffer-mode
+                cider-stacktrace-mode)
               evil-motion-state-modes))
-
-;;;;;;;;;;;;;;;
-;; Functions ;;
-;;;;;;;;;;;;;;;
-
-(defun p-open-clojure-docs ()
-  (interactive)
-  (browse-url "http://clojure.github.com"))
-
-(defun p-drawbridge-connect (url user passwd)
-  (interactive (list
-                (read-from-minibuffer
-                 "URL: "
-                 (format "https://%s.herokuapp.com/repl"
-                         (current-project-name)))
-                (read-from-minibuffer "User: ")
-                (read-passwd "Password: ")))
-  (let* ((url-components (split-string url "://"))
-         (url (car (last url-components)))
-         (protocol (if (= (length url-components) 1)
-                       "https"
-                     (car url-components)))
-         (connection-string (format "lein repl :connect %s://%s:%s@%s"
-                                    protocol
-                                    user
-                                    passwd
-                                    url)))
-    (message "Connecting to drawbridge...")
-    (run-lisp connection-string)))
-
-;;;;;;;;;;;
-;; Hooks ;;
-;;;;;;;;;;;
-
-(defun p-set-up-nrepl ()
-  (nrepl-turn-on-eldoc-mode))
-
-(defun p-set-up-nrepl-repl ()
-  (paredit-mode 1))
-
-(add-hook 'nrepl-interaction-mode-hook 'p-set-up-nrepl)
-(add-hook 'nrepl-mode-hook 'p-set-up-nrepl)
-(add-hook 'nrepl-mode-hook 'p-set-up-nrepl-repl)
-
-;;;;;;;;;;;;;;;;;
-;; Keybindings ;;
-;;;;;;;;;;;;;;;;;
-
-(p-configure-feature nrepl
-  (define-key nrepl-interaction-mode-map
-    (kbd "C-c C-c")
-    'nrepl-eval-buffer))
 
 ;;;;;;;;;;;;;;;;;
 ;; Workarounds ;;
