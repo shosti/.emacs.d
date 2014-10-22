06bbfe23f04ee80a3e0ac4c567aeb11427f1ffeb
Emanuel Evans <emanuel.evans@gmail.com>
2014-10-22 16:44:38 -0700

    Add wgrep

--- personal/p-ag.el
+++ personal/p-ag.el
@@ -1,11 +1,14 @@
 ;;; -*- lexical-binding: t -*-
 
+(p-require-package 'wgrep 'melpa)
+(p-require-package 'wgrep-ag 'melpa)
 (p-require-package 'ag)
 
 (require 'p-leader)
 
 (p-configure-feature ag
-  (setq ag-highlight-search t))
+  (setq ag-highlight-search t)
+  (define-key ag-mode-map (kbd "C-x C-q") 'wgrep-change-to-wgrep-mode))
 
 (p-set-leader-key
   "a" 'ag-project
