7e58493cba2b09278475ef092a3c52bdbc76d6ac
Emanuel Evans <emanuel.evans@gmail.com>
2015-04-18 23:29:31 -0700

    Use stable version of evil

--- personal/p-evil.el
+++ personal/p-evil.el
@@ -6,7 +6,7 @@
 (p-require-package 'undo-tree 'gnu)
 (p-require-package 'goto-last-change 'melpa)
 (p-require-package 'goto-chg 'marmalade)
-(p-require-package 'evil 'melpa)
+(p-require-package 'evil)
 (p-require-package 'evil-paredit 'melpa)
 (p-require-package 'evil-matchit)
 (p-require-package 'evil-surround 'melpa)
