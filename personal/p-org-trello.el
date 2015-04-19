11fdfb836bb596c142ae7ceedfe790606c801227
Emanuel Evans <emanuel.evans@gmail.com>
2015-04-19 00:33:07 -0700

    Get rid of org-trello
    Way more trouble than it's worth

--- personal/p-org-trello.el
+++
@@ -1,9 +0,0 @@
-(p-require-package 'elnode 'marmalade)
-(p-require-package 'web 'marmalade)
-(p-require-package 'deferred)
-(p-require-package 'creole 'marmalade)
-(p-require-package 'org-trello)
-
-(provide 'p-org-trello)
-
-;;; p-org-trello.el ends here
