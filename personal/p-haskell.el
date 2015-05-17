cd58e80a5d0512989cbb38d112927a3dc2d4a701
Emanuel Evans <emanuel.evans@gmail.com>
2015-05-17 10:42:09 -0700

    A bit of haskell stuff

--- personal/p-haskell.el
+++ personal/p-haskell.el
@@ -2,4 +2,9 @@
 
 (p-require-package 'haskell-mode)
 
+(add-hook 'haskell-mode-hook 'p-set-up-haskell-mode)
+
+(defun p-set-up-haskell-mode ()
+  (turn-on-haskell-indentation))
+
 (provide 'p-haskell)
