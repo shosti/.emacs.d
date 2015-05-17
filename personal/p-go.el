b38d440afe86f9f7873ab6f97129acae0c6b6ad4
Emanuel Evans <emanuel.evans@gmail.com>
2015-05-17 10:41:58 -0700

    Useful go stuff

--- personal/p-go.el
+++ personal/p-go.el
@@ -1,8 +1,13 @@
 (p-require-package 'go-mode)
 
+(defun p-godoc-at-point ()
+  (interactive)
+  (godoc-at-point (point)))
+
 (defun p-set-up-go ()
   (setq-local before-save-hook '(gofmt))
-  (setq-local tab-width 4))
+  (setq-local tab-width 4)
+  (setq-local evil-lookup-func #'p-godoc-at-point))
 
 (add-hook 'go-mode-hook #'p-set-up-go)
 
