cab95829da020682f0003c9272543fec4b2e201d
Emanuel Evans <emanuel.evans@gmail.com>
2015-05-26 17:10:05 -0700

    Using EWW a little bit

--- personal/p-eww.el
+++ personal/p-eww.el
@@ -5,6 +5,12 @@
 ;; Fix horrible eww coloring
 (setq shr-color-visible-luminance-min 70)
 
+(with-eval-after-load 'eww-mode
+  (evil-define-key 'motion eww-mode-map
+    "H" 'eww-back-url
+    "L" 'eww-forward-url
+    (kbd "SPC") 'ace-jump-char-mode))
+
 (provide 'p-eww)
 
 ;;; p-eww.el ends here
