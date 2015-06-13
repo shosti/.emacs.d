24c48f9115e02ca3061e54b79644cbd31637f829
Emanuel Evans <emanuel.evans@gmail.com>
2015-06-12 21:43:19 -0700

    Yay! Smart auto-complete for Go!

--- personal/p-go.el
+++ personal/p-go.el
@@ -1,4 +1,7 @@
+(require 'p-company)
+
 (p-require-package 'go-mode)
+(p-require-package 'company-go)
 
 (defun p-godoc-at-point ()
   (interactive)
@@ -7,7 +10,10 @@
 (defun p-set-up-go ()
   (setq-local before-save-hook '(gofmt))
   (setq-local tab-width 4)
-  (setq-local evil-lookup-func #'p-godoc-at-point))
+  (setq-local evil-lookup-func #'p-godoc-at-point)
+  (company-mode-on)
+  (setq-local company-backends '(company-go))
+  (electric-pair-mode 1))
 
 (add-hook 'go-mode-hook #'p-set-up-go)
 
