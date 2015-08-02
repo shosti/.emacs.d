cd77068aeeb6be3952a17d67f452f355c3bc2326
Emanuel Evans <emanuel.evans@gmail.com>
2015-08-02 14:08:05 -0700

    hjkl bindings for gist-list-mode

--- personal/p-gist.el
+++ personal/p-gist.el
@@ -1,5 +1,11 @@
 (p-require-package 'gist)
 
+(require 'p-evil)
+
+(with-eval-after-load 'gist
+  (p-add-hjkl-bindings gist-list-menu-mode-map 'emacs
+    "K" #'gist-kill-current))
+
 (provide 'p-gist)
 
 ;;; p-gist.el ends here
