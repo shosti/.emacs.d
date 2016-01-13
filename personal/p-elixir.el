c7b689b5f466ae0bb13f16f1de6aa7be17e6b4be
Emanuel Evans <emanuel.evans@gmail.com>
2016-01-13 09:49:35 -0800

    A little elixir tweaking

--- personal/p-elixir.el
+++ personal/p-elixir.el
@@ -2,6 +2,21 @@
 (p-require-package 'alchemist)
 (p-require-package 'ac-alchemist)
 
+(add-to-list 'auto-mode-alist
+             '("\\.eex\\'" . web-mode))
+
+(p-configure-feature elixir-mode
+  (add-to-list 'elixir-mode-hook #'p-config-elixir-end-mode)
+  (add-to-list 'elixir-mode-hook #'alchemist-mode))
+
+(setq alchemist-key-command-prefix (kbd "C-c C-a"))
+
+(defun p-config-elixir-end-mode ()
+  (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
+       "\\(?:^\\|\\s-+\\)\\(?:do\\)")
+  (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
+  (ruby-end-mode +1))
+
 (provide 'p-elixir)
 
 ;;; p-elixir.el ends here
