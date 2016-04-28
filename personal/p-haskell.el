7def5367bd10f4b945ca1f989e7df8b3140949d3
Emanuel Evans <mail@emanuel.industries>
2016-04-28 15:21:39 -0700

    Haskell stuff

--- personal/p-haskell.el
+++ personal/p-haskell.el
@@ -1,10 +1,14 @@
 ;;; -*- lexical-binding: t -*-
 
 (p-require-package 'haskell-mode)
+(p-require-package 'ghc)
+(p-require-package 'company-ghc)
 
-(add-hook 'haskell-mode-hook 'p-set-up-haskell-mode)
+(add-hook 'haskell-mode-hook #'p-set-up-haskell-mode)
 
 (defun p-set-up-haskell-mode ()
-  (turn-on-haskell-indentation))
+  (turn-on-haskell-indentation)
+  (ghc-init)
+  (setq-local company-backends (cons 'company-ghc company-backends)))
 
 (provide 'p-haskell)
