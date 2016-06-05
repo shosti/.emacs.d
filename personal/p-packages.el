79a45efc0cce11f58f246ebef510a33d94db3e6a
Emanuel Evans <mail@emanuel.industries>
2016-06-05 15:14:44 -0700

    Adieu marmalade
    👋

--- personal/p-packages.el
+++ personal/p-packages.el
@@ -9,8 +9,7 @@
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("melpa-stable" . "https://stable.melpa.org/packages/")
         ("melpa" . "https://melpa.org/packages/")
-        ("org" . "http://orgmode.org/elpa/")
-        ("marmalade" . "http://marmalade-repo.org/packages/")))
+        ("org" . "http://orgmode.org/elpa/")))
 
 (package-initialize)
 
