a458afd799584afe91edd398465767203247722d
Emanuel Evans <mail@emanuel.industries>
2016-08-27 21:02:14 -0700

    Quiet mode! 😯😯😯😯😯😯

---
+++ personal/p-quiet.el
@@ -0,0 +1,32 @@
+(require 'p-leader)
+
+(define-minor-mode p-quiet-mode
+  "Turn off notifications for ERC and mail."
+  :lighter " 😯" :global t
+  (if p-quiet-mode
+      (p-quiet-mode-on)
+    (p-quiet-mode-off)))
+
+(p-set-leader-key "q" #'p-quiet-mode)
+
+(defun p-quiet-mode-on ()
+  "Enable quiet mode."
+  (when (featurep 'erc)
+    (customize-set-variable 'erc-modules (delq 'track erc-modules)))
+  (when display-time-mode
+    (setq display-time-mail-string "")
+    (display-time-mode 1))
+  (message "Quiet mode enabled."))
+
+(defun p-quiet-mode-off ()
+  "Disable quiet mode."
+  (when (featurep 'erc)
+    (customize-set-variable 'erc-modules (cons 'track erc-modules)))
+  (when display-time-mode
+    (setq display-time-mail-string "✉")
+    (display-time-mode 1))
+  (message "Quiet mode disabled."))
+
+(provide 'p-quiet)
+
+;;; p-quiet.el ends here
