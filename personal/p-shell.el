40394763da58722e0ac57259119f7a2c9c3ce82d
Emanuel Evans <mail@emanuel.industries>
2017-05-03 13:07:32 -0700

    Flycheck for shell scripts

--- personal/p-shell.el
+++ personal/p-shell.el
@@ -7,4 +7,9 @@
 
 (global-set-key (kbd "C-z C-t") 'ansi-term)
 
+(defun p-set-up-shell-script-mode ()
+  (flycheck-mode 1))
+
+(add-hook 'sh-mode-hook #'p-set-up-shell-script-mode)
+
 (provide 'p-shell)
