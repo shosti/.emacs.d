f830ad452aa866193a1c77b5d83ae25349346707
Emanuel Evans <mail@emanuel.industries>
2017-05-03 13:05:25 -0700

    Just require erlang

--- personal/p-erlang.el
+++ personal/p-erlang.el
@@ -1,9 +1,4 @@
-(let ((erl-dir "/usr/lib/erlang/lib/tools-2.8.2/emacs"))
-  (when (file-directory-p erl-dir)
-    (add-to-list 'load-path erl-dir)
-    (require 'erlang-start)
-    (add-to-list 'auto-mode-alist
-                 '("\\.erl\\'" . erlang-mode))))
+(p-require-package 'erlang)
 
 (provide 'p-erlang)
 
