d4ca58c22ae8089e9ffd10ce12b5a4733bdb7f71
Emanuel Evans <mail@emanuel.industries>
2017-05-03 13:10:05 -0700

    Nuke pretty-mode

--- personal/p-octave.el
+++ personal/p-octave.el
@@ -2,7 +2,5 @@
 
 (setq inferior-octave-startup-file (concat user-emacs-directory ".emacs-octave"))
 (setenv "GNUTERM" "x11")
-(add-hook 'inferior-octave-mode-hook
-          'pretty-mode-on)
 
 (provide 'p-octave)
