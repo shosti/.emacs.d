0b57da0d687c4f90fa23deade11b26f4ce1f1cc9
Emanuel Evans <mail@emanuel.industries>
2018-02-10 16:06:36 -0800

    Emms attempts (can't remember what)

--- personal/p-emms.el
+++ personal/p-emms.el
@@ -3,44 +3,13 @@
 (require 'p-evil)
 (require 's)
 
-(defvar p-emms-mpd-extra-protocols
-  '("spotify:" "yt:" "youtube:" "pandora:" "http://"))
-
-(setq emms-info-functions '(emms-info-mpd)
-      emms-player-list '(emms-player-mpd))
-
-(autoload 'emms "emms" nil t nil)
+(setq emms-source-file-default-directory (expand-file-name "~/Music"))
 
 (with-eval-after-load 'emms
   (require 'emms-setup)
-  (require 'emms-player-mpd)
-  (require 'emms-mode-line)
-
-  ;; Minor hack to support spotify and other non-standard playlists.
-  (setq emms-player-mpd-supported-regexp
-        (concat (s-join "\\|" (mapcar (lambda (protocol)
-                                        (concat "\\`" protocol))
-                                      p-emms-mpd-extra-protocols))
-                "\\|"
-                (emms-player-simple-regexp
-                 "m3u" "ogg" "flac" "mp3" "wav" "mod" "au" "aiff")))
-  (emms-player-set emms-player-mpd
-                   'regex emms-player-mpd-supported-regexp)
-  (p-add-hjkl-bindings emms-playlist-mode-map 'emacs
-    "g" #'emms-player-mpd-connect)
-  (emms-mode-line 1)
-  (emms-standard)
-  (emms-player-mpd-connect))
-
-(defun p-browse-mopify ()
-  "Browse mopify web frontend for mopidy."
-  (interactive)
-  (browse-url "http://localhost:6680/mopify"))
-
-(defun p-browse-moped ()
-  "Browse moped web frontend for mopidy."
-  (interactive)
-  (browse-url "http://localhost:6680/moped"))
+  (emms-all)
+  (emms-default-players)
+  (p-add-hjkl-bindings emms-playlist-mode-map 'emacs))
 
 (p-set-leader-key
   "Mm" #'emms
