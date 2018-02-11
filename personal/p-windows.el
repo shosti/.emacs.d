bf73849f34881d1e23e02f145fd144efd134aa3e
Emanuel Evans <mail@emanuel.industries>
2018-02-10 16:08:14 -0800

    Semi-working git setup on windows

--- personal/p-windows.el
+++ personal/p-windows.el
@@ -1,7 +1,8 @@
 (when (eq system-type 'windows-nt)
   ;; Go-specific hacks
   (add-to-list 'exec-path (expand-file-name "~/go/bin"))
-  (setenv "PATH" (mapconcat 'identity exec-path ";")))
+  (setenv "PATH" (mapconcat 'identity exec-path ";"))
+  (setenv "GIT_SSH" "plink.exe"))
 
 (provide 'p-windows)
 
