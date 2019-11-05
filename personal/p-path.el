171367d58af756a5f5e33cc3a449d5d9bd4b4887
Emanuel Evans <mail@emanuel.industries>
2019-11-05 07:21:46 -0800

    Make path-from-file more robust

--- personal/p-path.el
+++ personal/p-path.el
@@ -4,14 +4,15 @@
   (defun p-path-from-file (fpath)
     "Reads a :-delineated path file and adds
 it to exec-path and the PATH variable"
-    (with-temp-buffer
-      (insert-file-contents fpath)
-      (setq exec-path
-            (append (-map (lambda (s)
-                            (s-trim (s-replace "$HOME" (expand-file-name "~") s)))
-                          (split-string (buffer-string) ":" t))
-                    exec-path))
-      (setenv "PATH" (mapconcat 'identity exec-path ":"))))
+    (when (file-exists-p fpath)
+      (with-temp-buffer
+        (insert-file-contents fpath)
+        (setq exec-path
+              (append (-map (lambda (s)
+                              (s-trim (s-replace "$HOME" (expand-file-name "~") s)))
+                            (split-string (buffer-string) ":" t))
+                      exec-path))
+        (setenv "PATH" (mapconcat 'identity exec-path ":")))))
   (defun p-update-path ()
     (interactive)
     (p-path-from-file "~/.path"))
