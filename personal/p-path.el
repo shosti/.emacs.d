6a8ff1a34d70f6cd6b7b2b74e4be47474c89460a
Emanuel Evans <mail@emanuel.industries>
2019-11-08 12:44:49 -0800

    Make path handling more manual and boring
    Seems like all these tools have fancy-schmancy bash hooks instead of just a
    manual PATH hack.

--- personal/p-path.el
+++ personal/p-path.el
@@ -1,26 +1,15 @@
 ;;; -*- lexical-binding: t -*-
 
 (unless (eq system-type 'windows-nt)
-  (defun p-path-from-file (fpath)
-    "Reads a :-delineated path file and adds
-it to exec-path and the PATH variable"
-    (when (file-exists-p fpath)
-      (with-temp-buffer
-        (insert-file-contents fpath)
-        (setq exec-path
-              (append (-map (lambda (s)
-                              (s-trim (s-replace "$HOME" (expand-file-name "~") s)))
-                            (split-string (buffer-string) ":" t))
-                      exec-path))
-        (setenv "PATH" (mapconcat 'identity exec-path ":")))))
-  (defun p-update-path ()
-    (interactive)
-    (p-path-from-file "~/.path"))
+  (defvar path-extra
+    '("~/.cargo/bin"
+      "~/.asdf/bin"
+      "~/.asdf/shims"
+      "~/.poetry/bin"))
 
-  (p-update-path))
-
-
-
-
+  (setq exec-path
+        (append (mapcar #'expand-file-name path-extra)
+                exec-path))
+  (setenv "PATH" (mapconcat 'identity exec-path ":")))
 
 (provide 'p-path)
