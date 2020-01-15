ba73e00aa39bf438273a6a9fee256330fc25ca94
Emanuel Evans <mail@emanuel.industries>
2020-01-15 14:45:03 -0800

    Automatically use minizinc-mode

--- personal/p-minizinc.el
+++ personal/p-minizinc.el
@@ -1,6 +1,9 @@
 ;;; -*- lexical-binding: t -*-
 (p-require-package 'minizinc-mode 'melpa)
 
+(add-to-list 'auto-mode-alist '("\\.mzn\\'" . minizinc-mode))
+(add-to-list 'auto-mode-alist '("\\.dzn\\'" . minizinc-mode))
+
 (provide 'p-minizinc)
 
 ;;; p-minizinc.el ends here
