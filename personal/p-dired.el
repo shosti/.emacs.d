e2ccaf4146d32e66fb8941e7fbe737edd792d413
Emanuel Evans <me@eevans.co>
2020-07-23 17:19:01 -0700

    Add shortcut to attach things to org in dired

--- personal/p-dired.el
+++ personal/p-dired.el
@@ -2,6 +2,7 @@
 
 (require 'p-evil)
 (require 'p-discover)
+(require 'p-org)
 (require 'dired)
 (require 'dired-x)
 
@@ -9,7 +10,14 @@
   (evil-define-key 'normal dired-mode-map (kbd "?")
     'makey-key-mode-popup-dired)
   (define-key dired-mode-map (kbd "M-o") nil)
-  (define-key dired-mode-map (kbd "C-x o") 'dired-omit-mode))
+  (define-key dired-mode-map (kbd "C-x o") 'dired-omit-mode)
+  (add-hook 'dired-mode-hook #'p-dired-setup))
+
+(defun p-dired-setup ()
+  (define-key dired-mode-map
+    (kbd "C-c C-x a")
+    #'org-attach-dired-to-subtree)
+  (turn-on-gnus-dired-mode))
 
 (provide 'p-dired)
 
