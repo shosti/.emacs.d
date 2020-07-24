2ed11f320169cdf468ad89062faaa48d4d379926
Emanuel Evans <me@eevans.co>
2020-07-23 17:31:40 -0700

    Get typescript setup working-ish
    Tide seems like a nice idea, but needs more tweaking before it's useful.

--- personal/p-typescript.el
+++ personal/p-typescript.el
@@ -1,33 +1,21 @@
 ;;; -*- lexical-binding: t -*-
-(p-require-package 'tide 'melpa)
 (p-require-package 'ts-comint 'melpa)
 (p-require-package 'typescript-mode 'melpa)
-
-(defun p-setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  (company-mode +1))
+(p-require-package 'add-node-modules-path 'melpa)
+(p-require-package 'prettier-js 'melpa)
 
 (defun p-set-up-web-mode-tsx ()
   (when (string-equal "tsx" (file-name-extension buffer-file-name))
-    (setup-tide-mode)))
+    (prettier-js-mode 1)))
 
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
 
 (with-eval-after-load 'web-mode
-  (add-hook 'web-mode-hook #'p-set-up-web-mode-tsx)
-  (flycheck-add-mode 'typescript-tslint 'web-mode))
+  (add-hook 'web-mode-hook #'add-node-modules-path)
+  (add-hook 'web-mode-hook #'p-set-up-web-mode-tsx))
 
 (with-eval-after-load 'typescript-mode
-  (add-hook 'typescript-mode-hook #'p-setup-tide-mode))
-
-;; formats the buffer before saving
-(with-eval-after-load 'tide
-  (add-hook 'before-save-hook #'tide-format-before-save))
+  (add-hook 'typescript-mode-hook #'prettier-js-mode))
 
 (provide 'p-typescript)
 
