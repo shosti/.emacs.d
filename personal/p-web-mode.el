e5dc997f026f6e3d83319afcf2838ed27c57c55d
Emanuel Evans <me@eevans.co>
2020-12-14 14:34:48 -0800

    Fix some annoyances with web-mode

--- personal/p-web-mode.el
+++ personal/p-web-mode.el
@@ -1,8 +1,9 @@
-(p-require-package 'web-mode)
+(p-require-package 'web-mode 'melpa)
 
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 
-(defun p-set-up-web-mode ())
+(setq web-mode-enable-auto-quoting nil
+      web-mode-code-indent-offset 2)
 
 (add-hook 'web-mode-hook 'p-set-up-web-mode)
 
