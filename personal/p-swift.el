37d2a4e40207df6df99bb58bdfa2a78e47d1c84f
Emanuel Evans <me@eevans.co>
2021-09-09 18:29:40 -0700

    Get some swift auto-formatting going

--- personal/p-swift.el
+++ personal/p-swift.el
@@ -11,11 +11,31 @@
     (setq lsp-sourcekit-executable
           (string-trim (shell-command-to-string "xcrun --find sourcekit-lsp")))))
 
-(defun p-set-up-swift ()
-  (when (eq system-type 'darwin)))
+(defun p-swift-format-buffer ()
+  "Rewrite current buffer in a canonical format using swift-format."
+  (interactive)
+  (when (executable-find "swift-format")
+    (let* ((config-arg
+            (when-let ((dir (locate-dominating-file buffer-file-name ".swift-format")))
+              (list "--configuration" (concat (expand-file-name dir) ".swift-format"))))
+           (buf (get-buffer-create "*swift-format*"))
+           (args `(,(point-min) ,(point-max) "swift-format" nil ,buf nil "format" ,@config-arg)))
+      (if (zerop (apply #'call-process-region args))
+          (let ((point (point))
+                (window-start (window-start)))
+            (erase-buffer)
+            (insert-buffer-substring buf)
+            (goto-char point)
+            (set-window-start nil window-start))
+        (message "swift-format: %s" (with-current-buffer buf (buffer-string))))
+      (kill-buffer buf))))
 
-(when (eq system-type 'darwin)
-  (add-hook 'swift-mode-hook #'lsp))
+(defun p-set-up-swift ()
+  (when (eq system-type 'darwin)
+    (lsp)
+    (add-hook 'before-save-hook #'p-swift-format-buffer)))
+
+(add-hook 'swift-mode-hook #'p-set-up-swift)
 
 (provide 'p-swift)
 
