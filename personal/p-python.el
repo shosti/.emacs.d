dd7f7d0d386d134c2f0ec097f8092b33e03e18c7
Emanuel Evans <me@eevans.co>
2021-09-13 15:07:36 -0700

    Put some nice defaults for jupyter blocks

--- personal/p-python.el
+++ personal/p-python.el
@@ -5,6 +5,10 @@
 (p-require-package 'websocket 'melpa)
 (p-require-package 'jupyter 'melpa)
 
+(setq org-babel-default-header-args:jupyter '((:async . "yes")
+                                              (:session . "/jpy:localhost#8888:python3")
+                                              (:kernel . "python")))
+
 (with-eval-after-load 'org
   (org-babel-do-load-languages 'org-babel-load-languages
                                '((emacs-lisp . t)
