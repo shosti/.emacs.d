74729a4556b1d9fa32b2d217f677f3adfcad1170
Emanuel Evans <me@eevans.co>
2021-09-13 11:07:22 -0700

    Get rid of references to jedi
    Can't remember if it ever worked, but it sure doesn't now...

--- personal/p-python.el
+++ personal/p-python.el
@@ -5,10 +5,7 @@
 (add-to-list 'evil-motion-state-modes 'ein:notebooklist-mode)
 
 (defun p-set-up-python-mode ()
-  (electric-indent-mode 0)
-  (hack-local-variables) ; necessary to get local python interpreter
-  (set (make-local-variable 'jedi:server-command)
-       (list python-shell-interpreter jedi:server-script)))
+  (electric-indent-mode 0))
 
 (defun p-py-newline-and-indent ()
   (interactive)
@@ -17,8 +14,6 @@
     (newline-and-indent)))
 
 (with-eval-after-load 'python-mode
-  (setq jedi:setup-keys t)
-  (add-hook 'python-mode-hook 'jedi:setup)
   (add-hook 'python-mode-hook 'p-set-up-python-mode)
   (define-key python-mode-map (kbd "RET") 'p-py-newline-and-indent))
 
