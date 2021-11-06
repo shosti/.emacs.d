47687037bbcb646b89b44898dfb0d50bf87d6f26
Emanuel Evans <me@eevans.co>
2021-11-06 08:43:04 -0700

    Some rust customization

--- personal/p-rust.el
+++ personal/p-rust.el
@@ -1,6 +1,11 @@
 (p-require-package 'rust-mode 'melpa)
 (p-require-package 'toml-mode 'melpa)
 
+(add-to-list 'exec-path (expand-file-name "~/.cargo/bin"))
+
+;; sadly rustfmt seems a bit buggy...
+(setq rust-format-on-save nil)
+
 (provide 'p-rust)
 
 ;; p-rust.el ends here
