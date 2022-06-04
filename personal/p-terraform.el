f2443c5437a60709888651322e4dc4e9e186d039
Emanuel Evans <me@eevans.co>
2022-06-04 15:32:13 -0700

    Try out terraform eglot
    Not 100% convinced but seems to maybe work?

--- personal/p-terraform.el
+++ personal/p-terraform.el
@@ -2,6 +2,9 @@
 
 (p-require-package 'terraform-mode)
 
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs '(terraform-mode . ("terraform-ls" "serve"))))
+
 (with-eval-after-load 'terraform-mode
   (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode))
 
