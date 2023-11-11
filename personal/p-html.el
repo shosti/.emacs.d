60fc5f5a1793ba8e9b226183b9672e4477bd615a
Emanuel Evans <me@eevans.co>
2023-11-10 21:21:22 -0800

    Fix extremely annoying mhtml keybinding

--- personal/p-html.el
+++ personal/p-html.el
@@ -3,6 +3,9 @@
 (with-eval-after-load 'html-mode
   (define-key html-mode-map (kbd "RET") 'newline-and-indent))
 
+(with-eval-after-load 'mhtml-mode
+  (define-key mhtml-mode-map (kbd "M-o") nil))
+
 (provide 'p-html)
 
 ;;; p-html.el ends here
