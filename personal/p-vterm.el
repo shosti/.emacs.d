b21750c328e4d4ba9cc34a78bd7294b9eb643b82
Emanuel Evans <me@eevans.co>
2024-03-11 18:13:36 -0700

    Update vterm configuration

--- personal/p-vterm.el
+++ personal/p-vterm.el
@@ -1,8 +1,18 @@
 (p-require-package 'vterm 'melpa)
 
 (require 'p-org)
+(require 'p-leader)
 
 (add-to-list 'evil-emacs-state-modes 'vterm-mode)
+
+(setq vterm-max-scrollback 10000)
+
+(with-eval-after-load 'vterm
+  (mapcar #'(lambda (k)
+              (define-key vterm-mode-map (kbd k) nil))
+          '("M-0" "M-1" "M-2" "M-3")))
+
+(p-set-leader-key "Z" #'vterm)
 
 (provide 'p-vterm)
 
