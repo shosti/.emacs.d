bbe084d1cad79007a71c554d87cadaa1b32cd10d
Emanuel Evans <me@eevans.co>
2024-03-11 18:28:41 -0700

    Experimental treesitter config

--- personal/p-treesitter.el
+++ personal/p-treesitter.el
@@ -1,3 +1,13 @@
+(setq treesit-language-source-alist
+      '((yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
+(unless (package-installed-p 'combobulate)
+  (package-vc-install "https://github.com/mickeynp/combobulate"))
+
+(unless (package-installed-p 'treesit-fold)
+  (package-vc-install "https://github.com/abougouffa/treesit-fold"))
+
+(use-package combobulate)
 
 (provide 'p-treesitter)
 
--- personal/p-yaml.el
+++ personal/p-yaml.el
@@ -1,5 +1,8 @@
 (require 'p-programming)
 
+(setq major-mode-remap-alist
+      (cons '(yaml-mode . yaml-ts-mode) major-mode-remap-alist))
+
 (use-package yaml-ts-mode
   :init
   (add-hook 'yaml-ts-mode-hook #'p-set-up-yaml))
