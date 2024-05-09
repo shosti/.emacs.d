fe8ac0793c39c37c4d3be8be5f9d28915ab741f7
Emanuel Evans <emanuel@ycombinator.com>
2024-05-09 10:51:44 -0700

    Add tsx tree-sitter support

--- personal/p-treesitter.el
+++ personal/p-treesitter.el
@@ -1,6 +1,7 @@
 (setq treesit-language-source-alist
       '((yaml "https://github.com/ikatyang/tree-sitter-yaml")
-        (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")))
+        (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+        (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")))
 
 (unless (package-installed-p 'combobulate)
   (package-vc-install "https://github.com/mickeynp/combobulate"))
